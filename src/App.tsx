bf24715156741ee02b2d865c848ec56286f63636
hoyeoun <jay08111@naver.com>
2024-09-22 14:28:38 +0900

    # ADD - router set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,49 @@
-import { useEffect } from "react";
-import { Requester } from "./apis/caller";
-import { Button } from "@/components/ui/button";
+import { lazy } from "react";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  Outlet,
+} from "react-router-dom";
 
-function App() {
-  const call = async () => {
-    const t = await Requester.GET("v1/users");
+const Login = lazy(() => import("@/pages/Login/LoginPage"));
 
-    console.log(t);
-  };
+const Home = () => <h2>Home Page</h2>;
+const NotFound = () => <h2>404 - Not Found</h2>;
 
-  useEffect(() => {
-    call();
-  }, []);
+const AuthTokenRoute = ({ isAuthenticated = false }) => {
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" replace />;
+  }
+  return <Outlet />;
+};
 
-  return <Button onClick={() => console.log("hey?")}> Click me! </Button>;
-}
+const ProtectedRoute = ({ isAuthenticated = false }) => {
+  if (isAuthenticated) {
+    return <Outlet />;
+  }
+  return <Navigate to="/login" replace />;
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route element={<AuthTokenRoute isAuthenticated={true} />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+
+        <Route element={<ProtectedRoute isAuthenticated={true} />}>
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
 export default App;
--- src/apis/caller.ts
+++ src/apis/caller.ts
@@ -5,25 +5,23 @@ interface RequestHeader extends AxiosRequestHeaders {
   "api-lang": string;
 }
 
-console.log(import.meta);
-
 export class Requester {
   static header: RequestHeader;
 
   private static _baseURL = import.meta.env.VITE_BASE_URL;
 
-  public static async GET(url: string) {
+  static async GET(url: string) {
     const { data, status } = await axios.get(`${this._baseURL}/${url}`, {});
 
     return { data, status };
   }
-  public static async POST(url: string, data: unknown) {
+  static async POST(url: string, data: unknown) {
     return await axios.post(`${this._baseURL}/${url}`, data, {});
   }
-  public static async PUT(url: string, data: unknown) {
+  static async PUT(url: string, data: unknown) {
     return await axios.put(`${this._baseURL}/${url}`, data, {});
   }
-  public static async DELETE(url: string) {
+  static async DELETE(url: string) {
     return await axios.delete(`${this._baseURL}/${url}`, {});
   }
 }
